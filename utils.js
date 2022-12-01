24b3ecd9ca209ffc4ebc2cdc7b3fe77c5f32ada8
jeremy-then <jeremy.then@iovlabs.org>
2022-12-01 18:44:40 -0400

    Adds some documentation

--- utils.js
+++ utils.js
@@ -42,17 +42,27 @@ const isValidNetwork = (network) => {
  * 
  */
  const signedNumberToHexStringLE = number => {
+    // `Math.log2` is useful to get the count of significant bits in a number, - 1.
+    // If number has the binary 101010 which has 6 significant bits, Math.log2(number) would return 5 (6 - 1).
+    // This happens because the `Math.log2` function does an operation similar to dividing number by 2 until it reaches 1,
+    // which yields the number of significant bits, minus 1, due to the power of 2 nature of binary.
     const bitCount = Math.log2(number);
     const byteLength = parseInt(bitCount / COUNT_OF_BITS_IN_BYTE + 1);
     const oneByteMask = 0xFF;
     const byteArray = [];
 
     for(let i = 0; i < byteLength; i++) {
+        // right-shifts `number` by `i * COUNT_OF_BITS_IN_BYTE` to bring the next byte to be the first byte,
+        // copies it and adds it to `byteArray`, and keeps doing that until all bytes are copied.
+        // When `i` is 0, then `i * COUNT_OF_BITS_IN_BYTE` is 0 and hence `number` does not git shifted and we copy the first byte.
         const nextByte = (number >> (i * COUNT_OF_BITS_IN_BYTE)) & oneByteMask;
         byteArray.push(nextByte);
     }
 
     const mostSignificantBitPosition = byteLength * COUNT_OF_BITS_IN_BYTE - 1;
+    // Checks if the most significant bit (MSB) is on (1) by left-shifting 1 to take it to
+    // the post significant bit and do an `&` bitwise operation. If the result is greater than 0,
+    // then the bit is on.
     const mostSignificantBitIsOn = (number & (1 << mostSignificantBitPosition)) > 0;
 
     if (mostSignificantBitIsOn) {
