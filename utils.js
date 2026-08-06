4a727cc985ab40f336bf605f01bc30cbdf7179d5
Marcos <marcos@rootstocklabs.com>
2026-08-06 12:06:11 -0300

    Remove bitcoinjs-lib dependency. Implement only the needed methods

--- utils.js
+++ utils.js
@@ -1,4 +1,29 @@
-const opcodes = require('bitcoinjs-lib').script.OPS;
+// Bitcoin script opcode values (BIP-62 / Script wiki), hardcoded so this library
+// doesn't need a Bitcoin script-encoding dependency for a handful of stable constants.
+const OPS = {
+    OP_1: 0x51,
+    OP_2: 0x52,
+    OP_3: 0x53,
+    OP_4: 0x54,
+    OP_5: 0x55,
+    OP_6: 0x56,
+    OP_7: 0x57,
+    OP_8: 0x58,
+    OP_9: 0x59,
+    OP_10: 0x5a,
+    OP_11: 0x5b,
+    OP_12: 0x5c,
+    OP_13: 0x5d,
+    OP_14: 0x5e,
+    OP_15: 0x5f,
+    OP_16: 0x60,
+    OP_NOTIF: 0x64,
+    OP_ELSE: 0x67,
+    OP_ENDIF: 0x68,
+    OP_DROP: 0x75,
+    OP_CHECKSEQUENCEVERIFY: 0xb2,
+    OP_CHECKMULTISIG: 0xae
+};
 
 const numberToHexString = (number) => number.toString(16);
 const hexToDecimal = hex => parseInt(hex, 16);
@@ -8,22 +33,22 @@ const ONE_BYTE_MASK = 0xFF;
 const ONE_BIT_MASK = 0x01;
 
 const decimalToOpCode = {
-    1: opcodes.OP_1,
-    2: opcodes.OP_2,
-    3: opcodes.OP_3,
-    4: opcodes.OP_4,
-    5: opcodes.OP_5,
-    6: opcodes.OP_6,
-    7: opcodes.OP_7,
-    8: opcodes.OP_8,
-    9: opcodes.OP_9,
-    10: opcodes.OP_10,
-    11: opcodes.OP_11,
-    12: opcodes.OP_12,
-    13: opcodes.OP_13,
-    14: opcodes.OP_14,
-    15: opcodes.OP_15,
-    16: opcodes.OP_16
+    1: OPS.OP_1,
+    2: OPS.OP_2,
+    3: OPS.OP_3,
+    4: OPS.OP_4,
+    5: OPS.OP_5,
+    6: OPS.OP_6,
+    7: OPS.OP_7,
+    8: OPS.OP_8,
+    9: OPS.OP_9,
+    10: OPS.OP_10,
+    11: OPS.OP_11,
+    12: OPS.OP_12,
+    13: OPS.OP_13,
+    14: OPS.OP_14,
+    15: OPS.OP_15,
+    16: OPS.OP_16
 }
 
 /**
@@ -60,6 +85,7 @@ const decimalToOpCode = {
 };
 
 module.exports = {
+    OPS,
     numberToHexString,
     hexToDecimal,
     decimalToOpCode,
